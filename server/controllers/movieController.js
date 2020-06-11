d2de34ed14ca3117d79d1f3aac1c2306a4124e88
unknown <sonymartha4@gmail.com>
2020-06-12 00:21:21 +0700

    add film with poster, and fix error 404 when run random film

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -2,19 +2,33 @@ const axios = require('axios')
 
 class movieController{
     static show (req,res) {
-        const random = Math.floor(Math.random() * 1000) + 100; 
-        axios.get(`https://api.themoviedb.org/3/movie/${random}`, {
-            params: {
-                api_key: '5182b94d862b722b16c7c4930eeb14a2',
-                language: 'en-US'
-            }
-        })
-        .then(data => {
-            res.status(200).json(data.data)
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
+        const randomFilm = Math.floor(Math.random() * 10) + 1; 
+        const randomPage = Math.floor(Math.random() * 100) + 1;
+        var film = {}
+            axios.get(`https://api.themoviedb.org/3/discover/movie?`, {
+                params: {
+                    api_key: '5182b94d862b722b16c7c4930eeb14a2',
+                    language: 'en-US',
+                    sort_by: 'popularity.desc',
+                    page: randomPage
+                }
+            })
+            .then(data => {
+                var title = data.data.results[randomFilm].title
+                title = title.split(' ').join('+')
+                console.log(title)
+                axios.get(`http://www.omdbapi.com/?t=${title}&apikey=30edb524`)
+                .then(resp => {
+                    film = resp.data
+                    res.status(200).json(film) 
+                })
+            })
+            
+            .catch(err => {
+                res.status(500).json(err)
+            })
+        
+        
     }
 }
 
